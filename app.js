12ac23089686a16d9dbe6010eb7d94287b574827
yangjiqiang <1005473728@qq.com>
2019-04-01 14:13:18 +0800

    update: 允许跨域访问

--- app.js
+++ app.js
@@ -13,9 +13,7 @@ var app = express();
 
 app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Methods', '*');
-  res.header('Content-Type', 'application/json;charset=utf-8');
   next();
 });
 
